8de8556ae1d0843f762a88bffbb92f3b01223022
longhongc <longhongc@gmail.com>
2021-02-20 21:32:34 +0800

    Add JoyStick README

---
+++ joystick/README.md
@@ -0,0 +1,8 @@
+# JoyStick
+
+# Wiring
+
+
+# Demo
+
+- Pico has 12 bits adc, but the function read_u16 returns 16 bits Integer from 0 ~ 65535  
--- joystick/joystick_example.py
+++ joystick/joystick_example.py
@@ -1,13 +1,13 @@
-import machine
+from machine import Pin, ADC
 import utime
 
-x_pin = machine.ADC(26)
-y_pin = machine.ADC(27)
-b_pin = machine.Pin(22, machine.Pin.IN, machine.Pin.PULL_DOWN)
+x_pin = ADC(26)
+y_pin = ADC(27)
+b_pin = Pin(22, Pin.IN, Pin.PULL_DOWN)
 
 while True:
     print("X vlaue is {}".format(x_pin.read_u16()))
     print("Y vlaue is {}".format(y_pin.read_u16()))
     if b_pin==1:
-        print("attack")
+        print("button pressed")
     utime.sleep(0.5)
